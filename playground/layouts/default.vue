ac00d60b0881ea99f2849b101e1ec8218a506f09
Kloen <kloen@kloenlansfiel.com>
2024-05-01 21:08:58 +0200

    chore: Update playground layout

--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <UNavMenu>
-    <template #menu-items>
-      <UNavMenuItem href="/button"> Button </UNavMenuItem>
-      <UNavMenuItem href="/card"> Card </UNavMenuItem>
-      <UNavMenuItem href="/dialog"> Dialog </UNavMenuItem>
-      <UNavMenuItem href="/heading"> Heading </UNavMenuItem>
-      <UNavMenuItem href="/skeleton"> Skeleton </UNavMenuItem>
-      <UNavMenuItem href="/switch"> Switch </UNavMenuItem>
-      <UNavMenuItem href="/tooltip"> Tooltip </UNavMenuItem>
-    </template>
-  </UNavMenu>
-  <UContainer :constrained="true" class="flex flex-col gap-4">
-    <slot></slot>
+  <header>
+    <UNavMenu>
+      <template #menu-items>
+        <UNavMenuItem href="/button"> Button </UNavMenuItem>
+        <UNavMenuItem href="/card"> Card </UNavMenuItem>
+        <UNavMenuItem href="/dialog"> Dialog </UNavMenuItem>
+        <UNavMenuItem href="/heading"> Heading </UNavMenuItem>
+        <UNavMenuItem href="/skeleton"> Skeleton </UNavMenuItem>
+        <UNavMenuItem href="/switch"> Switch </UNavMenuItem>
+        <UNavMenuItem href="/tooltip"> Tooltip </UNavMenuItem>
+      </template>
+    </UNavMenu>
+  </header>
+  <UContainer :constrained="true">
+    <main class="flex flex-col gap-4">
+      <slot></slot>
+    </main>
   </UContainer>
 </template>
