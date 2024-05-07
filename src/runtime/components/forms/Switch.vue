1e0e4a55a84132b0bee8fca5ea4b8248680bc196
Kloen <kloen@kloenlansfiel.com>
2024-05-07 17:36:19 +0200

    feat: Switch switch from emits to model

--- src/runtime/components/forms/Switch.vue
+++ src/runtime/components/forms/Switch.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { SwitchRoot, SwitchThumb } from 'radix-vue'
-import { computed, ref } from "vue"
+import { SwitchRoot, SwitchThumb } from "radix-vue"
+import { computed, defineModel } from "vue"
 
-const switchState = ref(false)
-const emit = defineEmits(["updated"])
-
-const update = () => {
-  switchState.value = !switchState.value
-  emit("updated", switchState.value)
-}
+const model = defineModel({
+  default: false,
+  required: true,
+  type: Boolean
+})
 
 const rootClass = computed(() => {
-  if (switchState.value) {
+  if (model.value) {
     return "bg-blue-800"
   } else {
     return "bg-neutral-800"
@@ -19,7 +17,7 @@ const rootClass = computed(() => {
 })
 
 const thumbClass = computed(() => {
-  if (switchState.value) {
+  if (model.value) {
     return "translate-x-5"
   } else {
     return "translate-none"
@@ -29,11 +27,20 @@ const thumbClass = computed(() => {
 
 <template>
   <SwitchRoot
-    v-model:checked="switchState"
-    :class="[rootClass, 'relative inline-block w-10 h-5 transition-colors duration-400 ease-in-out block rounded-full'].join(' ')"
+    v-model:checked="model"
+    :class="
+      [rootClass, 'relative inline-block w-10 h-5 transition-colors duration-400 ease-in-out block rounded-full'].join(
+        ' '
+      )
+    "
   >
     <SwitchThumb
-      :class="[thumbClass, 'before:content-empty absolute block h-3 w-3 left-1 bottom-1 bg-white transition-transform duration-400 ease-in-out transform rounded-full'].join(' ')"
+      :class="
+        [
+          thumbClass,
+          'before:content-empty absolute block h-3 w-3 left-1 bottom-1 bg-white transition-transform duration-400 ease-in-out transform rounded-full'
+        ].join(' ')
+      "
     />
   </SwitchRoot>
 </template>
