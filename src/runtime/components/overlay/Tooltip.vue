84c75047486cae1ca44e8a526e6049225184f345
Kloen <kloen@kloenlansfiel.com>
2024-05-10 11:03:48 +0200

    fix: Tooltip z position

--- src/runtime/components/overlay/Tooltip.vue
+++ src/runtime/components/overlay/Tooltip.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { TooltipArrow, TooltipContent, TooltipPortal, TooltipProvider, TooltipRoot, TooltipTrigger } from 'radix-vue'
+import { TooltipArrow, TooltipContent, TooltipPortal, TooltipProvider, TooltipRoot, TooltipTrigger } from "radix-vue"
 import type { PropType } from "vue"
 
 type PositionType = "right" | "left" | "top" | "bottom"
@@ -7,7 +7,7 @@ type PositionType = "right" | "left" | "top" | "bottom"
 defineProps({
   position: {
     type: String as PropType<PositionType>,
-    default: () => 'top'
+    default: () => "top"
   }
 })
 </script>
@@ -19,11 +19,7 @@ defineProps({
         <slot></slot>
       </TooltipTrigger>
       <TooltipPortal>
-        <TooltipContent
-          class="rounded-lg bg-neutral-800 px-2 py-1"
-          :side-offset="5"
-          :side="position"
-        >
+        <TooltipContent class="rounded-lg bg-neutral-800 px-2 py-1 z-100" :side-offset="5" :side="position">
           <slot name="tooltip"></slot>
           <TooltipArrow class="fill-neutral-800" :width="8" />
         </TooltipContent>
