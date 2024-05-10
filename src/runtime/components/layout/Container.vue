dd2cd6740c54ff1374930d39917413b5096169ce
Kloen <kloen@kloenlansfiel.com>
2024-05-10 10:42:28 +0200

    feat: Extend container

--- src/runtime/components/layout/Container.vue
+++ src/runtime/components/layout/Container.vue
@@ -1,16 +1,30 @@
 <script lang="ts" setup>
-import type { PropType } from "vue"
+import { computed, type PropType } from "vue"
 
-defineProps({
+const props = defineProps({
   is: {
-    type: String as PropType<"div" | "form" | "main">,
+    type: String as PropType<"div" | "form" | "main" | "article">,
     default: () => "div"
+  },
+  layout: {
+    type: String as PropType<"vertical" | "horizontal">,
+    default: () => "vertical"
+  }
+})
+
+const containerClass = computed(() => {
+  const baseClass = "flex gap-4 mx-auto px-4 sm:px-6 lg:px-8 w-full"
+  switch (props.layout) {
+    case "horizontal":
+      return baseClass
+    case "vertical":
+      return ["flex-col", baseClass].join(" ")
   }
 })
 </script>
 
 <template>
-  <component :is class="mx-auto px-4 sm:px-6 lg:px-8 w-full">
+  <component :is :class="containerClass">
     <slot></slot>
   </component>
 </template>
