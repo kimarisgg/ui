f1c32a9c7ea63923b673e9982ac0148ee9896b68
Kloen <kloen@kloenlansfiel.com>
2024-05-10 13:38:08 +0200

    chore: Update playground

--- playground/pages/skeleton.vue
+++ playground/pages/skeleton.vue
@@ -1,6 +1,6 @@
 <template>
-  <USkeleton extra-class="h-16 rounded-lg" />
-  <USkeleton extra-class="h-8 rounded-md" />
-  <USkeleton extra-class="h-16 rounded-full" />
+  <USkeleton extra-class="h-16 w-32 rounded-lg" />
+  <USkeleton extra-class="h-8 w-12 rounded-md" />
+  <USkeleton extra-class="h-16 w-full rounded-full" />
   <USkeleton extra-class="h-16 w-32 rounded-full" />
 </template>
