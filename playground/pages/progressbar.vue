8a5363e143bb3f0c27ad625472c115703aa964b5
Kloen <kloen@kloenlansfiel.com>
2024-05-13 17:58:54 +0200

    feat: ProgressBar

---
+++ playground/pages/progressbar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const progress = ref<number>(0)
+</script>
+
+<template>
+  <div class="flex gap-4 justify-center">
+    <UButton type="button" @click="progress = 0">0</UButton>
+    <UButton type="button" @click="progress = 25">25</UButton>
+    <UButton type="button" @click="progress = 50">50</UButton>
+    <UButton type="button" @click="progress = 75">75</UButton>
+    <UButton type="button" @click="progress = 100">100</UButton>
+  </div>
+  <div class="flex gap-4 justify-center">
+    <UButton type="button" @click="progress += 1">+1</UButton>
+    <UButton type="button" @click="progress += 10">+10</UButton>
+    <UButton type="button" @click="progress += 100">+100</UButton>
+  </div>
+  <UProgressBar v-model="progress"></UProgressBar>
+</template>
---
+++ src/runtime/components/elements/ProgressBar.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const model = defineModel({
+  type: Number,
+  get(v) {
+    if (v > 100) {
+      return 100
+    } else {
+      return v
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="relative overflow-hidden rounded-lg w-full h-0.5 bg-neutral-800">
+    <span
+      class="h-full absolute left-0 top-0 bg-emerald-600 w-full transition duration-300 ease-in-out"
+      :style="{ transform: `translateX(${model - 100}%)` }"
+    />
+  </div>
+</template>
